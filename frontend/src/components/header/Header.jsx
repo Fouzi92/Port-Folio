71749a6f9a1f66f97bf233f5ed3a0e5bc544d2ea
fouzi92git config --global user.email fouzi457@hotmail.frgit config --global user.name fouzi92 <fouzi457@hotmail.fr>
2022-05-10 17:58:00 +0200

    suite portfolio css fini

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import "../header/header.css"
+import Cta from './Cta';
 
 const Header = () => {
     return (
-        <div>
-           connard
-        </div>
+       <header>
+           <div className="container header__container">
+          <h5>Bonjour Je suis </h5>
+          <h1>Un chomeur</h1>
+          <h5 className='text-light'>Fullstack galérien</h5>
+          <Cta />
+
+           </div>
+       </header>
+       
     );
 };
 
--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -4,12 +4,7 @@ import "../nav/nav.css"
 const Nav = () => {
     return (
         <div className='NavBar'>
-            <li> 
-                <ul>sssssssssssssssssssssssssssssssssssssssssss</ul>
-                <ul>ddddddddddddddddddddddddddddddddd</ul>
-                <ul>cccccccccccccccccccccccccccccccccccccccccccc</ul>
-            </li>
-            
+         
         </div>
     );
 };
