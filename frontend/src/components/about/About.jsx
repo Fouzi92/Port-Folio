c959e605718a62b038d4e327d19117ec652f60bb
fouzi92git config --global user.email fouzi457@hotmail.frgit config --global user.name fouzi92 <fouzi457@hotmail.fr>
2022-05-12 13:02:48 +0200

    about en cours

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -1,10 +1,52 @@
-import React from 'react';
-import '../about/about.css'
+import React from "react";
+import "../about/about.css";
+import moi2 from "../../assets/moi-2.jpg";
+import {FiAward} from "react-icons/fi"
+import {FiUserCheck} from "react-icons/fi"
+import {BiFolderOpen} from "react-icons/bi"
 
 const About = () => {
     return (
-       <section id='about'> About</section>
-    );
+        <section id="about">
+            <h5> Apprendre à me connaître</h5>
+            <h2> Sur moi </h2>
+
+            <div className="container about__container">
+                <div className="about__me">
+                    <div className="about__me-image">
+                        <img src={moi2} alt="moi le bg" />
+                    </div>
+                </div>
+                <div className="about__content">
+                    <div className="about__cards">
+                        <article className="about__card">
+                            <FiAward className="about__icon"/>
+                            <h5>Expérience</h5>
+                            <small>6mois de formation </small>
+                        </article>
+
+                        <article className="about__card">
+                            <FiUserCheck className="about__icon"/>
+                            <h5>Client</h5>
+                            <small>lool des Pigeons Wallah </small>
+                        </article>
+
+                        <article className="about__card">
+                            <BiFolderOpen className="about__icon"/>
+                            <h5>Projets</h5>
+                            <small>10 réaliser</small>
+                        </article>
+
+                    </div>
+
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio fuga facilis quo ullam aperiam esse, vitae architecto obcaecati numquam eaque voluptatem totam quas quaerat odit explicabo ab provident? Deleniti, sint?</p>
+
+                    <a href="#contact" className="btn btn-promary"> Parlons </a>
+                </div>
+            </div>
+        </section>
+    );  
+       
 };
 
 export default About;
--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -5,15 +5,17 @@ import {BiUserVoice} from "react-icons/bi"
 import {FiBookOpen} from "react-icons/fi"
 import {RiServiceLine} from "react-icons/ri"
 import {BiMessageRoundedCheck} from "react-icons/bi"
+import { useState } from 'react';
 
 const Nav = () => {
+    const [activeNav, setActiveNav] = useState("#")
     return (
        <nav>
-           <a href="#header" className='active'><BiHomeSmile/></a>
-            <a href="#about"><BiUserVoice/></a>
-             <a href="#experience"><FiBookOpen/></a>
-              <a href="#services"><RiServiceLine/></a>
-               <a href="#contact"><BiMessageRoundedCheck/></a>
+           <a href="#header" className={activeNav === '#' ? 'active' : ''}><BiHomeSmile/></a>
+            <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ""}><BiUserVoice/></a>
+             <a href="#experience"  onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ""}><FiBookOpen/></a>
+              <a href="#services"  onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ""}><RiServiceLine/></a>
+               <a href="#contact"  onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ""}><BiMessageRoundedCheck/></a>
        </nav>
     );
 };
