1ea9f94159ef342ce69404e4990bec1b387e3fe3
fouzi92git config --global user.email fouzi457@hotmail.frgit config --global user.name fouzi92 <fouzi457@hotmail.fr>
2022-05-13 17:55:47 +0200

    temoignage css en cour

--- frontend/src/components/testimonials/Testimonials.jsx
+++ frontend/src/components/testimonials/Testimonials.jsx
@@ -5,6 +5,33 @@ import AVTR2 from "../../assets/avatar2.jpg"
 import AVTR3 from "../../assets/avatar3.jpg"
 import AVTR4 from "../../assets/avatar4.jpg"
 
+const data = [
+{
+avatar: AVTR1,
+Name:' Tina Snow',
+review: 'Lorem ipsum doloamet consectetur adipisicing elit. Dolorem sapiente possimus obcaecati sint vel, architecto doloremque esse alias nihil corrupti harum provident ea, voluptatibus quidem delectus ratione, distinctio placeat? Incidunt.'
+},
+
+{
+avatar: AVTR2,
+Name:' Shatta Wale',
+review: 'Lorem ipsum doloamet consectetur adipisicing elit. Dolorem sapiente possimus obcaecati sint vel, architecto doloremque esse alias nihil corrupti harum provident ea, voluptatibus quidem delectus ratione, distinctio placeat? Incidunt.'
+},
+
+{
+avatar: AVTR3,
+Name:' Kwame Despite',
+review: 'Lorem ipsum doloamet consectetur adipisicing elit. Dolorem sapiente possimus obcaecati sint vel, architecto doloremque esse alias nihil corrupti harum provident ea, voluptatibus quidem delectus ratione, distinctio placeat? Incidunt.'
+},
+
+{
+avatar: AVTR4,
+Name:' Eric Zemmour',
+review: 'Lorem ipsum doloamet consectetur adipisicing elit. Dolorem sapiente possimus obcaecati sint vel, architecto doloremque esse alias nihil corrupti harum provident ea, voluptatibus quidem delectus ratione, distinctio placeat? Incidunt.'
+}
+
+]
+
 const Testimonials = () => {
     return (
         <section id='testimonials'>
@@ -12,56 +39,25 @@ const Testimonials = () => {
               <h2>Témoignages "En vrais c'est du MITO Wallah</h2>
 
               <div className="container testimonials__container">
-                 <article className='testimonial'> 
+               {
+                   data.map(({avatar, name, review}, index) => {
+                       return (
+                 <article key={index} className='testimonial'> 
                    <div className="client__avatar">
-                     <img src={AVTR1} alt="Avatar" />
+                     <img src={avatar} alt="r"/>
                      </div>
-                     <h5 className="client__name"> Ernest Achiever</h5>
-                     <small className='client__review'>
-                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempore tenetur dolor mollitia architecto, sequi nesciunt temporibus, est repellat nihil harum, quo saepe dicta. Dolores quae iusto facere distinctio tempore.
+                     <h5 className="client__name"> {name}</h5>
+                     <small className='client__review'> {review}
+                      
                      </small>
                 
                   
                 </article>
+                       )
+                   })
+               }
 
-                <article className='testimonial'> 
-                   <div className="client__avatar">
-                      <img src={AVTR2} alt="Avatar" />
-                        <h5 className="client__name"> Ernest Achiever</h5>
-                           </div>
-                           <small className='client__review'>
-                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempore tenetur dolor mollitia architecto, sequi nesciunt temporibus, est repellat nihil harum, quo saepe dicta. Dolores quae iusto facere distinctio tempore.
-                     </small>
-                
-                  
-                </article>
-
-                <article className='testimonial'> 
-                 <div className="client__avatar">
-                    <img src={AVTR3} alt="Avatar" />
-                     <h5 className="client__name"> Ernest Achiever</h5>
-                     </div>
-                        <small className='client__review'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempore tenetur dolor mollitia architecto, sequi nesciunt temporibus, est repellat nihil harum, quo saepe dicta. Dolores quae iusto facere distinctio tempore.
-                     </small>
-                    
-                </article>
-
-                <article className='testimonial'> 
-                 <div className="client__avatar">
-                    <img src={AVTR4} alt="Avatar" />
-                     <h5 className="client__name"> Ernest Achiever</h5>
-                     </div>
-                        <small className='client__review'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempore tenetur dolor mollitia architecto, sequi nesciunt temporibus, est repellat nihil harum, quo saepe dicta. Dolores quae iusto facere distinctio tempore.
-                     </small>
-                    
-                </article>
-
-                     
-                
-               
-              </div>
+            </div>
         </section>
     );
 };
---
+++ frontend/src/components/testimonials/data.jsx
@@ -0,0 +1,56 @@
+<section id='testimonials'>
+             <h5>Avis clients "Pigeon"</h5>
+              <h2>Témoignages "En vrais c'est du MITO Wallah</h2>
+
+              <div className="container testimonials__container">
+                 <article className='testimonial'> 
+                   <div className="client__avatar">
+                     <img src={AVTR1} alt="Avatar" />
+                     </div>
+                     <h5 className="client__name"> Ernest Achiever</h5>
+                     <small className='client__review'>
+                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempore tenetur dolor mollitia architecto, sequi nesciunt temporibus, est repellat nihil harum, quo saepe dicta. Dolores quae iusto facere distinctio tempore.
+                     </small>
+                
+                  
+                </article>
+
+                <article className='testimonial'> 
+                   <div className="client__avatar">
+                      <img src={AVTR2} alt="Avatar" />
+                        <h5 className="client__name"> Ernest Achiever</h5>
+                           </div>
+                           <small className='client__review'>
+                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempore tenetur dolor mollitia architecto, sequi nesciunt temporibus, est repellat nihil harum, quo saepe dicta. Dolores quae iusto facere distinctio tempore.
+                     </small>
+                
+                  
+                </article>
+
+                <article className='testimonial'> 
+                 <div className="client__avatar">
+                    <img src={AVTR3} alt="Avatar" />
+                     <h5 className="client__name"> Ernest Achiever</h5>
+                     </div>
+                        <small className='client__review'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempore tenetur dolor mollitia architecto, sequi nesciunt temporibus, est repellat nihil harum, quo saepe dicta. Dolores quae iusto facere distinctio tempore.
+                     </small>
+                    
+                </article>
+
+                <article className='testimonial'> 
+                 <div className="client__avatar">
+                    <img src={AVTR4} alt="Avatar" />
+                     <h5 className="client__name"> Ernest Achiever</h5>
+                     </div>
+                        <small className='client__review'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempore tenetur dolor mollitia architecto, sequi nesciunt temporibus, est repellat nihil harum, quo saepe dicta. Dolores quae iusto facere distinctio tempore.
+                     </small>
+                    
+                </article>
+
+                     
+                
+               
+              </div>
+        </section>
