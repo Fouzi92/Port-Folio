c9017d5024df46b8b87698c71edc737f8cb1052b
fouzi92git config --global user.email fouzi457@hotmail.frgit config --global user.name fouzi92 <fouzi457@hotmail.fr>
2022-05-13 13:07:15 +0200

    portfolio css en cour

--- frontend/src/components/experience/Experience.jsx
+++ frontend/src/components/experience/Experience.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "../experience/experience.css"
-import {BsCheckSquare} from "react-icons/bs"
+//import {BsCheckSquare} from "react-icons/bs"
+import { SiCheckmarx } from "react-icons/si"
 
 const Experience = () => {
     return (
@@ -12,39 +13,51 @@ const Experience = () => {
                     <h3>Développement front-end</h3>
                     <div className="experience__content">
                         <article className='experience__details'>
-                            <BsCheckSquare/>
+                            <SiCheckmarx className='experience__details-icon' />
+                            <div>
                             <h4>HTML</h4>
                             <small className='text-light'>Avancé</small>
+                            </div>
                         </article>
                         
                         <article className='experience__details'>
-                            <BsCheckSquare/>
+                            <SiCheckmarx className='experience__details-icon'/>
+                            <div>
                             <h4>CSS</h4>
                             <small className='text-light'>Avancé</small>
+                            </div>
                         </article>
                         
                         <article className='experience__details'>
-                            <BsCheckSquare/>
+                            <SiCheckmarx className='experience__details-icon'/>
+                            <div>
                             <h4>JavaScript</h4>
                             <small className='text-light'>Intermédiaire</small>
+                            </div>
                         </article>
                         
                         <article className='experience__details'>
-                            <BsCheckSquare/>
+                            <SiCheckmarx className='experience__details-icon'/>
+                            <div>
                             <h4>SASS</h4>
                             <small className='text-light'>Intermédiaire</small>
+                            </div>
                         </article>
                        
                         <article className='experience__details'>
-                            <BsCheckSquare/>
+                            <SiCheckmarx className='experience__details-icon'/>
+                            <div> 
                             <h4>React</h4>
                             <small className='text-light'>Intermédiaire</small>
+                            </div>
                         </article>
                         
                         <article className='experience__details'>
-                            <BsCheckSquare/>
+                            <SiCheckmarx className='experience__details-icon'/>
+                            <div>
                             <h4>Glandeur</h4>
                             <small className='text-light'>Expert</small>
+                            </div>
                         </article>
 
                     </div>
@@ -56,39 +69,51 @@ const Experience = () => {
                  <h3>Développement Back-end</h3>
                     <div className="experience__content">
                         <article className='experience__details'>
-                            <BsCheckSquare/>
+                            <SiCheckmarx className='experience__details-icon'/>
+                            <div> 
                             <h4>Node JS</h4>
                             <small className='text-light'>Débutant</small>
+                            </div>
                         </article>
                         
                         <article className='experience__details'>
-                            <BsCheckSquare/>
+                            <SiCheckmarx className='experience__details-icon'/>
+                            <div> 
                             <h4>MongoDB</h4>
                             <small className='text-light'>Débutant</small>
+                            </div>
                         </article>
                         
                         <article className='experience__details'>
-                            <BsCheckSquare/>
+                            <SiCheckmarx className='experience__details-icon'/>
+                            <div>
                             <h4>NoSQL</h4>
                             <small className='text-light'>Débutant</small>
+                            </div>
                         </article>
                         
                         <article className='experience__details'>
-                            <BsCheckSquare/>
+                            <SiCheckmarx className='experience__details-icon'/>
+                            <div>
                             <h4>JAVA</h4>
                             <small className='text-light'>Débutant</small>
+                            </div>
                         </article>
                        
                         <article className='experience__details'>
-                            <BsCheckSquare/>
+                            <SiCheckmarx className='experience__details-icon'/>
+                            <div> 
                             <h4>PHP</h4>
                             <small className='text-light'>Débutant</small>
+                            </div>
                         </article>
                         
                         <article className='experience__details'>
-                            <BsCheckSquare/>
+                            <SiCheckmarx className='experience__details-icon'/>
+                            <div> 
                             <h4>Glandeur</h4>
                             <small className='text-light'>Expert</small>
+                            </div>
                         </article>
 
                     </div>
--- frontend/src/components/services/Services .jsx
+++ frontend/src/components/services/Services .jsx
@@ -1,9 +1,122 @@
 import React from 'react';
 import "../services/services.css"
+import {BsCheck2All} from "react-icons/bs"
 
 const Services  = () => {
     return (
-        <section id='services'>Services</section>
+        <section id='services'>
+             <h5>ce que j'offre</h5>
+            <h2> Mes Services</h2>
+            
+            <div className="container services__container">
+              <article className='service'>
+                  <div className="service__head">
+                      <h3>UI/UX Design</h3>
+                  </div>
+                  
+                  <ul className='service__list'>
+                    <li>
+                        <BsCheck2All className='service__list-icon'/>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p> 
+                        </li>
+                        <li>
+                        <BsCheck2All className='service__list-icon'/>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p> 
+                        </li>
+                        <li>
+                        <BsCheck2All className='service__list-icon'/>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p> 
+                        </li>
+                        <li>
+                        <BsCheck2All className='service__list-icon'/>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p> 
+                        </li>
+                        <li>
+                        <BsCheck2All className='service__list-icon'/>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p> 
+                        </li>
+                        <li>
+                        <BsCheck2All className='service__list-icon'/>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p> 
+                        </li>
+
+                  </ul>
+              </article>
+
+               {/* Ui-Ux fini */}
+
+                             <article className='service'>
+                  <div className="service__head">
+                      <h3>Développement Web</h3>
+                  </div>
+                  
+                  <ul className='service__list'>
+                    <li>
+                        <BsCheck2All className='service__list-icon'/>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p> 
+                        </li>
+                        <li>
+                        <BsCheck2All className='service__list-icon'/>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p> 
+                        </li>
+                        <li>
+                        <BsCheck2All className='service__list-icon'/>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p> 
+                        </li>
+                        <li>
+                        <BsCheck2All className='service__list-icon'/>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p> 
+                        </li>
+                        <li>
+                        <BsCheck2All className='service__list-icon'/>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p> 
+                        </li>
+                        <li>
+                        <BsCheck2All className='service__list-icon'/>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p> 
+                        </li>
+
+                  </ul>
+              </article>
+
+              {/* WeB DEVELOPMENT */ }
+
+                <article className='service'>
+                  <div className="service__head">
+                      <h3>Création de contenu</h3>
+                  </div>
+                  
+                  <ul className='service__list'>
+                    <li>
+                        <BsCheck2All className='service__list-icon'/>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p> 
+                        </li>
+                        <li>
+                        <BsCheck2All className='service__list-icon'/>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p> 
+                        </li>
+                        <li>
+                        <BsCheck2All className='service__list-icon'/>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p> 
+                        </li>
+                        <li>
+                        <BsCheck2All className='service__list-icon'/>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p> 
+                        </li>
+                        <li>
+                        <BsCheck2All className='service__list-icon'/>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p> 
+                        </li>
+                        <li>
+                        <BsCheck2All className='service__list-icon'/>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p> 
+                        </li>
+
+                  </ul>
+              </article>
+              { /* Création de contenu */}
+            </div>
+        </section>
     );
 };
 
